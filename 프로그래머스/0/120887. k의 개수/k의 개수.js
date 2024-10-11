b8b3ee40673933e12f6f999349e41d2271c4f164
Bae Moon Gue <bmgbm9@naver.com>
2024-10-12 02:08:22 +0900

    [level 0] Title: k의 개수, Time: 149.10 ms, Memory: 57.7 MB -BaekjoonHub

--- 프로그래머스/0/120887. k의 개수/k의 개수.js
+++ 프로그래머스/0/120887. k의 개수/k의 개수.js
@@ -6,17 +6,22 @@ function solution(i, j, k) {
     var count = 0;
     
     for(let b=i; b<=j; b++){
+        //arr1에 문자로 저장 ["1", "2", ... "10", "11"]
         arr1.push(String(b))
     }
     for(let c=0; c<arr1.length; c++) {
         var num1 = arr1[c].split("")
+        //num1 변수에다가 arr1[c]값을 split한다. ex) "123" ===> ["1", "2", "3"]
         for(let d=0; d<num1.length; d++){
+            //num1 배열을 arr2에 순서대로 push한다
+            //전부 한자리 숫자로 arr2에 들어간다.
             arr2.push(num1[d])
         }
     }
     
     for(let e=0; e<arr2.length; e++){
         if(arr2[e].match(regex)) {
+            //match함수로 k가 등장할때마다 count를 1씩 올린다
             count++;
         }
     }
